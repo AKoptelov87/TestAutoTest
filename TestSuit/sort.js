ba425a88a10e3bf2a7ad81337ae782d409902fbd
Anton.Koptelov <Anton.Koptelov@billing.ru>
2016-12-05 18:07:09 +0400

    Добавлено Задание 9. Проверить сортировку стран и геозон в админке

--- TestSuit/sort.js
+++ TestSuit/sort.js
@@ -9,7 +9,7 @@ var webdriver = require('selenium-webdriver'),
     assert = require('assert');
 
 test.describe('Task 7. Open all section of admin menu', function () {
-    var driver, numbersCountryWhithZones = [], error = [];
+    var driver;
 
     test.before(function () {
         driver = new webdriver.Builder()
@@ -44,108 +44,163 @@ test.describe('Task 7. Open all section of admin menu', function () {
         driver.wait(driver.findElement(by.css('td#sidebar')).isDisplayed(), 10000);
     });
 
-    test.it('Открываем страницу со странами', function () {
+    test.it('Открываем раздел меню Countries', function () {
         // driver.get('http://localhost/litecart/admin/?app=countries&doc=countries');
         driver.findElement(by.xpath('.//li[@id="app-"]/a/span[contains(text(),"Countries")]')).click();
         driver.findElement(by.xpath('//td[@id="content"]/h1[contains(text(),"Countries")]'));
     });
 
-    test.xit('Проверяем сортировку списка стран', function () {
-        chechTableSorting(driver.findElements(by.css('tr.row > td:nth-child(5) > a')));
-    });
-    test.xit('Читаем все страны и сравниваем с сортированным списком', function () {
-
+    test.it('Проверяем сортировку списка стран', function () {
         driver.findElements(by.css('tr.row > td:nth-child(5) > a'))
-            .then(function (allCountry) {
-                var countryNames = [];
-                console.log("Стран в списке: " + ( allCountry.length + 1));
-                allCountry.forEach(function (country) {
+            .then(function (allCountries) {
+                var sourceList = [];
+                console.log("В списке: " + ( allCountries.length ));
+                allCountries.forEach(function (country) {
                     country.getText()
                         .then(function (countryName) {
-                            countryNames.push(countryName);
-                            // countryNames.push('zz');
+                            sourceList.push(countryName);
+                            // sourceList.push('zz');  // для проверки
                         })
                 });
-                return countryNames
+                return sourceList;
             })
-            .then(function (countryNames) {
-                var sortedCountryNames = countryNames.slice().sort();
-                assert.deepEqual(countryNames, sortedCountryNames, 'Порядок нарушен!');
-                // for (i = 1; i < countryNames.length; i++) {
-                //     console.log(sortedCountryNames[i] +'  ===  '+ countryNames[i]);
-                //     assert((sortedCountryNames[i] !== countryNames[i]), 'Порядок нарушен!');
-                //     if (sortedCountryNames[i] !== countryNames[i]) {
-                //         console.log('Порядок нарушен!')
-                //     }
-                // }
+            .then(function (sourceList) {
+                var targetList = sourceList.slice().sort();
+                assert.deepEqual(sourceList, targetList, 'Порядок сортировки нарушен!');
+                console.log('Все зоны в алфавитном порядке');
             })
     });
 
     test.it('Находим страны с зонами', function () {
+        var error = [];
         driver.findElements(by.css('tr.row > td:nth-child(6)'))
             .then(function (geoCounts) {
-                var countryNumber = 1;
+                var countryNumber = 1,  //грязный хук
+                    namesCountryWhithZones = [];
                 geoCounts.forEach(function (geoCount) {
                     geoCount.getText()
                         .then(function (count) {
-                            countryNumber++;
-                            // console.log( 'нашли зон:' + count);
+                            countryNumber++ ;
                             if (count != 0) {
-                                console.log('В стране №' + countryNumber + ' нойдено ' + count + ' зон');
-                                numbersCountryWhithZones.push(countryNumber)
+                                driver.findElement(by.css('table.dataTable tr.row:nth-child(' + countryNumber + ') a')).getText()
+                                    .then(function (countryName) {
+                                        console.log('В стране ' + countryName + ' нойдено ' + count + ' зон');
+                                        namesCountryWhithZones.push(countryName)
+                                    })
                             }
                         });
-
                 });
-                return numbersCountryWhithZones;
+                return namesCountryWhithZones;
             })
-            .then(function (countryNumbers) {
-                console.log(countryNumbers);
-                countryNumbers.forEach(function (countryNumber) {
-                    driver.findElement(by.css('table.dataTable tr.row:nth-child(' + countryNumber + ') a')).click();
-                    if (
-                        chechTableSorting(driver.findElements(by.xpath('.//table[@id="table-zones"]/tbody/tr/td[3][input[@type="hidden"]]')))
-                    ) {
-                        error.push("errr");
-                    }
-                    driver.findElement(by.name('cancel')).click();
-                })
-
+            .then(function (countryNames) {
+                // console.log(countryNames);
+                countryNames.forEach(function (countryName) {
+                    driver.findElement(by.xpath('.//td/a[contains(text(), "' + countryName + '")]')).click();
+                    driver.findElements(by.xpath('.//table[@id="table-zones"]/tbody/tr/td[3][input[@type="hidden"]]'))
+                        .then(function (allZones) {
+                            var sourceList = [];
+                            console.log('Проверяем страну: ' + countryName);
+                            console.log("В списке: " + ( allZones.length ));
+                            allZones.forEach(function (zone) {
+                                zone.getText()
+                                    .then(function (zoneName) {
+                                        sourceList.push(zoneName);
+                                        // sourceList.push('zz');  // для отладки
+                                    })
+                            });
+                            return sourceList;
+                        })
+                        .then(function (sourceList) {
+                            var targetList = sourceList.slice().sort();
+                            for (i = 0; i < sourceList.length; i++) {
+                                console.log(sourceList[i] + '  ===  ' + targetList[i]);
+                                if (targetList[i] !== sourceList[i]) {
+                                    result = 'Порядок нарушен! Расхождение с элемента №' + (i + 1);
+                                    console.log(result);
+                                    error.push('Ошибка при проверке страны ' + countryName + ' ' + result);
+                                    break;
+                                }
+                            }
+                        })
+                        .then(function () {
+                            driver.findElement(by.name('cancel')).click();
+                        })
+                });
+                return error;
+            })
+            .then(function (error) {
+                if (error.length > 0) {
+                    console.log(error);
+                    assert.fail(error.length, 0, 'Присутсвуют ошибки в сортировке', '>');
+                } else {
+                    console.log('Все зоны в алфавитном порядке');
+                }
             })
 
     });
 
-    test.it('Проверяем наличие ошибок ', function () {
-        console.log(error);
-        assert.equal(error.length, 0);
+    test.it('Открываем раздел меню Geo Zones и запоминаем зоны', function () {
+        // driver.get('http://localhost/litecart/admin/?app=countries&doc=countries');
+        driver.findElement(by.xpath('.//li[@id="app-"]/a/span[contains(text(),"Geo Zones")]')).click();
+        driver.findElement(by.xpath('//td[@id="content"]/h1[contains(text(),"Geo Zones")]'));
+    });
+
+    test.it('Заходим в каждую зону и проверяем порядок', function () {
+        var error = [],
+            zoneNames = [];
+
+        driver.findElements(by.css('table.dataTable tr.row > td:nth-child(3) > a'))
+            .then(function (geoZones) {
+                geoZones.forEach(function (zone) {
+                    zone.getText()
+                        .then(function (zoneName) {
+                            zoneNames.push(zoneName);
+                        })
+                });
+                return zoneNames;
+            })
+            .then(function (zoneNames) {
+                zoneNames.forEach(function (zone) {
+                    driver.findElement(by.xpath('.//table[@class="dataTable"]//a[contains(text(),"' + zone + '")]')).click();
+                    driver.findElements(by.css('table#table-zones tr > td:nth-child(3) option[selected="selected"]'))
+                        .then(function (allZones) {
+                            var sourceList = [];
+                            console.log('Проверяем гео зону: ' + zone);
+                            console.log("В списке: " + ( allZones.length ));
+                            allZones.forEach(function (zone) {
+                                zone.getText()
+                                    .then(function (zoneName) {
+                                        sourceList.push(zoneName);
+                                        // sourceList.push('zz'); // для отладки
+                                    })
+                            });
+                            return sourceList;
+                        })
+                        .then(function (sourceList) {
+                            var targetList = sourceList.slice().sort();
+                            for (i = 0; i < sourceList.length; i++) {
+                                console.log(sourceList[i] + '  ===  ' + targetList[i]);
+                                if (targetList[i] !== sourceList[i]) {
+                                    result = 'Порядок нарушен! Расхождение с элемента №' + (i + 1);
+                                    console.log(result);
+                                    error.push('Ошибка при проверке зоны ' + zone + ' ' + result);
+                                    break;
+                                }
+                            }
+                        })
+                        .then(function () {
+                            driver.findElement(by.name('cancel')).click();
+                        })
+                });
+                return error;
+            })
+            .then(function (error) {
+                if (error.length > 0) {
+                    console.log(error);
+                    assert.fail(error.length, 0, 'Присутсвуют ошибки в сортировке', '>');
+                } else {
+                    console.log('Все зоны в алфавитном порядке');
+                }
+            })
     })
 });
-
-function chechTableSorting(table) {
-    table
-        .then(function (inputArray) {
-            var sourceList = [];
-            console.log("В списке: " + ( inputArray.length + 1));
-            inputArray.forEach(function (target) {
-                target.getText()
-                    .then(function (targetText) {
-                        sourceList.push(targetText);
-                        sourceList.push('zz');
-                    })
-            });
-            return sourceList;
-        })
-        .then(function (sourceList) {
-            var targetList = sourceList.slice().sort();
-            // assert.deepEqual(sourceList, targetList, 'Порядок нарушен!');
-            for (i = 0; i < sourceList.length; i++) {
-                console.log(sourceList[i] + '  ===  ' + targetList[i]);
-                if (targetList[i] !== sourceList[i]) {
-                    message = 'Порядок нарушен! Расхождение с элемента №' + (i+1);
-                    console.log(message);
-                    return message;
-                    // break;
-                }
-            }
-        })
-}
