ff4e1adf7b05ed659de153b3e042265b5fe21783
AKoptelov87 <AKoptelov87@gmail.com>
2016-12-19 22:23:42 +0400

    Задание 17. Проверьте отсутствие сообщений в логе браузера

--- TestSuit/checkBrowserLogs.js
+++ TestSuit/checkBrowserLogs.js
@@ -9,7 +9,7 @@ var webdriver = require('selenium-webdriver'),
     test = require('selenium-webdriver/testing'),
     assert = require('assert');
 
-test.describe('Task 7. Open all section of admin menu', function () {
+test.describe('Task 17. Check the messages in the browser log', function () {
     var driver, countOfMenu, error = [];
 
     test.before(function () {
@@ -26,7 +26,6 @@ test.describe('Task 7. Open all section of admin menu', function () {
         driver.get('http://localhost/litecart/admin/');
         driver.findElement(by.name('username'));
     });
-
     test.it('Авторизация админом', function () {
         var loginField = driver.findElement(by.name('username')),
             passField = driver.findElement(by.name('password')),
@@ -57,7 +56,24 @@ test.describe('Task 7. Open all section of admin menu', function () {
             });
     });
 
-    test.it('Авторизация админом', function () {});
+    test.it('прокликать уток', function () {
+        driver.findElements(by.css('table.dataTable td > img + a'))
+            .then(function (ducks) {
+                count = ducks.length;
+                console.log(count);
+                //так как все товары снизу - перебираем с конца, но последняя строка - футер
+                for (var i = 2; i <= count+1; i++){
+                    driver.findElement(by.css('table.dataTable tr:nth-last-child('+i+') img + a')).click();
+                    driver.findElement(by.name('cancel')).click();
+                }
+            })
+    });
 
-    test.it('Авторизация админом', function () {});
+    test.it('Проверка, что полученный лог браузера не содержит ошибок', function () {
+        driver.manage().logs().get('browser')
+            .then(function (browserLog) {
+                console.log('консолька браузера: '+browserLog);
+                assert.equal(browserLog.length, 0, 'В логе браузера существуют сообщения об ошибках');
+        });
+    });
 })
